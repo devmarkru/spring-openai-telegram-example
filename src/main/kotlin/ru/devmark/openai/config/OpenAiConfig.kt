3d33ba413acd9e5c20dfe90733a837c9e4f30e30
devmarkru <devmarkru@gmail.com>
2025-06-16 18:20:35 +0300

    custom chatMemory example

--- src/main/kotlin/ru/devmark/openai/config/OpenAiConfig.kt
+++ src/main/kotlin/ru/devmark/openai/config/OpenAiConfig.kt
@@ -4,11 +4,20 @@ import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor
 import org.springframework.ai.chat.memory.ChatMemory
+import org.springframework.ai.chat.memory.ChatMemoryRepository
+import org.springframework.ai.chat.memory.MessageWindowChatMemory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class OpenAiConfig {
+    @Bean
+    fun chatMemory(chatMemoryRepository: ChatMemoryRepository): ChatMemory =
+        MessageWindowChatMemory.builder()
+            .chatMemoryRepository(chatMemoryRepository) // InMemoryChatMemoryRepository
+            .maxMessages(100) // сохраняем последние 100 сообщений
+            .build()
+
     @Bean
     fun chatClient(builder: ChatClient.Builder, chatMemory: ChatMemory): ChatClient =
         builder
